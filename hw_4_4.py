723230c9247604870bc276a973b9e7fae6477404
Chekanov Artem <tema-42@mail.ru>
2023-03-12 23:27:31 +0700

    finished all tests

--- hw_4_4.py
+++ hw_4_4.py
@@ -1,3 +1,7 @@
+import math
+import random
+
+
 def test_greeting():
     """
     Напишите программу, которая выводит на экран приветствие.
@@ -9,6 +13,7 @@ def test_greeting():
     # Проверяем результат
     assert output == "Привет, Анна! Тебе 25 лет."
 
+
 def test_rectangle():
     """
     Напишите программу, которая берет длину и ширину прямоугольника
@@ -17,13 +22,12 @@ def test_rectangle():
     a = 10
     b = 20
     # TODO сосчитайте периметр
-    perimeter = 0
     perimeter = a + a + b + b
     assert perimeter == 60
     # TODO сосчитайте площадь
-    area = 0
     area = a * b
     assert area == 200
+
 
 def test_circle():
     """
@@ -32,20 +36,25 @@ def test_circle():
     """
     r = 23
     # TODO сосчитайте площадь
-    area = 0
+    area = math.pi * r * r
+    print(area)
     assert area == 1661.9025137490005
     # TODO сосчитайте длину окружности
-    length = 0
+    length = 2 * math.pi * r
+    print(length)
     assert length == 144.51326206513048
+
 
 def test_random_list():
     """
     Создайте список из 10 случайных чисел от 1 до 100 и отсортируйте его по возрастанию.
     """
     # TODO создайте список
-    l = []
+    l = [random.randint(1, 100) for i in range(10)]
+    l = sorted(l)
     assert len(l) == 10
     assert l[0] < l[-1]
+
 
 def test_unique_elements():
     """
@@ -53,9 +62,11 @@ def test_unique_elements():
     """
     l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
     # TODO удалите повторяющиеся элементы
+    l = list(set(l))
     assert isinstance(l, list)
     assert len(l) == 10
     assert l == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 
 def test_dicts():
     """
@@ -67,6 +78,6 @@ def test_dicts():
     first = ["a", "b", "c", "d", "e"]
     second = [1, 2, 3, 4, 5]
     # TODO создайте словарь
-    d = {}
+    d = dict(zip(first, second))
     assert isinstance(d, dict)
     assert len(d) == 5
--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
+import random
+from random import randint
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -14,3 +14,11 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def test_random_list():
+    """
+    Создайте список из 10 случайных чисел от 1 до 100 и отсортируйте его по возрастанию.
+    """
+    l = [randint(1, 100) for i in range(10)]
+    l = sorted(l)
+    assert len(l) == 10
+    assert l[0] < l[-1]
